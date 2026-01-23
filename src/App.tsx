863754f08931cdae122b1c8a94efc1379cd04d8f
jyjang <jinyoung_8965@naver.com>
2026-01-23 14:15:08 +0900

    feat: 구조 변경
    1. queryClient 의 위치를 index로 변경
    2. app.tsx에 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { queryClient } from "./providers/queryClient";
 import WeatherPage from "./pages/WeatherPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
-        <QueryClientProvider client={queryClient}>
-            <WeatherPage />
-        </QueryClientProvider>
+        <Routes>
+            {/* 메인 페이지 */}
+            <Route path="/" element={<WeatherPage />} />
+
+            {/* 즐겨찾기 상세 페이지 */}
+            <Route path="/weather/:lat/:lon" element={<WeatherPage />} />
+        </Routes>
     );
 }
 
