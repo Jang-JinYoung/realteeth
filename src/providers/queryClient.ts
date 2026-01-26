1f282d6c89a4e1895b50bcc3c8c91e3c7418ca1a
jyjang <jinyoung_8965@naver.com>
2026-01-26 16:32:05 +0900

    feat: query-client 수정

--- src/providers/queryClient.ts
+++ src/providers/queryClient.ts
@@ -3,8 +3,11 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            retry: 1,
+            retry: false,
             refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            staleTime: 1000 * 60 * 5, // 5분
+            gcTime: 1000 * 60 * 30, // 30분
         },
     },
 });
